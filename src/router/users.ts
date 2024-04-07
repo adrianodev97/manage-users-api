3a8d72a06e59f43fc87874c4b1947e29ecee1fc1
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2024-04-07 01:15:11 -0300

    feat: implement user update

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { getAllUsers, deleteUser } from '../controllers/users'
+import { getAllUsers, deleteUser, updateUser } from '../controllers/users'
 import { isAuthentication, isOwner } from '../middlewares'
 
 export default (router: express.Router) => {
   router.get('/users', isAuthentication, getAllUsers)
-  router.delete('/delete/:id', isAuthentication, isOwner, deleteUser,)
+  router.delete('/users/:id', isAuthentication, isOwner, deleteUser)
+  router.patch('/users/:id', isAuthentication, isOwner, updateUser)
 }
